e781e549e49e6ae7837428c523d82de9ba6f859a
Harsh Mantri <harshmantri98@gmail.com>
2018-02-24 00:07:07 +0530

    First working prototype

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # SpellCheck
 
 ##Features:
->Easy spell check so that you never misspell
+>Easy spell check so that you never make the same mistake twice
+
+>Create your own dictionary for all holding the self made words
 
 >Suggestions: Gives reliable suggestions
 
--- SpellList.py
+++ SpellList.py
@@ -1,2 +1,22 @@
-//A trial small scale version of the spell checker
+# A trial small scale version of the custom dictionary class
+Class myDictionary(object):
+    def __init__(self):
+        self.name= input('Enter the name of the dictionary file: ')
+        if not ".dict" in self.name:
+            self.name += '.dict'
+        self.dictionaryList=[]
 
+    def openDictionary(self):
+        self.fRead=open(self.name,'r')
+        for i in self.fRead:
+            self.dictionaryList.append(i.strip())
+
+    def addAWord(self,word):
+        self.dictionaryList.append(word)
+        self.dictionaryList.sort()
+
+    def IsWordInDictionary(self,word):
+        return word in self.dictionaryList
+
+    def closeDictionary(self):
+        self.fRead.close()
