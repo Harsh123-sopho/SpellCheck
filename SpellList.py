0a56e842e67eb1799c91b27fbc01d1028fb7908b
Harsh Mantri <harshmantri98@gmail.com>
2018-02-24 00:38:28 +0530

    Changes to the first basic code after a trial run

--- SpellList.py
+++ SpellList.py
@@ -1,10 +1,12 @@
 # A trial small scale version of the custom dictionary class
-Class myDictionary(object):
+class myDictionary(object):
     def __init__(self):
         self.name= input('Enter the name of the dictionary file: ')
         if not ".dict" in self.name:
             self.name += '.dict'
         self.dictionaryList=[]
+        self.fAppend=open(self.name,'a')
+        self.fAppend.close()
 
     def openDictionary(self):
         self.fRead=open(self.name,'r')
@@ -15,8 +17,14 @@ Class myDictionary(object):
         self.dictionaryList.append(word)
         self.dictionaryList.sort()
 
-    def IsWordInDictionary(self,word):
+    def isWordInDictionary(self,word):
         return word in self.dictionaryList
-
+    
+    def saveChanges(self):
+        self.fWrite=open(self.name,'w')
+        for i in self.dictionaryList:
+            self.fWrite.write(i+"\n")
+        self.fWrite.close()
+        
     def closeDictionary(self):
         self.fRead.close()
