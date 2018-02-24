1ae52f727bd5d83a02a78ab4abc42cf8b3dc7c0b
Harsh Mantri <harshmantri98@gmail.com>
2018-02-25 01:35:38 +0530

    Cooler look- multi-line print! :O

--- SpellChecker.py
+++ SpellChecker.py
@@ -4,12 +4,15 @@ from Scanner import *
 #This file has the necessary code for spell check
 
 if __name__=='__main__':
-    print('Welcome to the document spell checker!\nMake sure that your document and the personal dictionary, or the default one we have provided are in the same directory as this code.')
-    dictionary = myDictionary()
-    print('What do you plan to do: ')
-    print('1. Add a new word to your dictionary')
-    print('2. Scan a document for errors')
+    print('''
+Welcome to the document spell checker!\nMake sure that your document and the personal dictionary, or the default one we have provided are in the same directory as this code.
+What do you plan to do: 
+1. Add a new word to your dictionary
+2. Scan a document for errors
+''')
+
     n = input('Enter the task number for execution: ')
+    dictionary=myDictionary()
 
     if n == '1':
         dictionary.openDictionary()
